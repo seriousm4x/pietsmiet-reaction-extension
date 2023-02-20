d0d8b1c781c93c8a766ab137afa679afee16172f
Maxi Quoß <maxi@quoss.org>
2023-02-20 07:58:02 +0100

    first extension version

---
+++ extension/main.js
@@ -0,0 +1,114 @@
+let logPreTag = "[pietsmiet reactions]";
+let storageItem = localStorage.getItem("pietsmiet-reactions");
+
+function createStorageItem() {
+    // get videos from github
+    fetch(
+        "https://raw.githubusercontent.com/seriousm4x/pietsmiet-reaction-extension/main/data/matches.min.json"
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            storageItem = {
+                fetched_at: Date.now(),
+                videos: data,
+            };
+            localStorage.setItem(
+                "pietsmiet-reactions",
+                JSON.stringify(storageItem)
+            );
+        });
+}
+
+function createHTML() {
+    // only run on /watch url
+    if (window.location.pathname !== "/watch") {
+        return;
+    }
+
+    // get video id from url
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const videoId = urlParams.get("v");
+
+    // remove our container if it exists
+    if (document.querySelector("#pietsmiet-reaction-container")) {
+        document.querySelector("#pietsmiet-reaction-container").remove();
+    }
+
+    // create div
+    let aboveTheFold = document.querySelector("#above-the-fold");
+    const reactionBox = document.createElement("div");
+    reactionBox.id = "pietsmiet-reaction-container";
+    reactionBox.style.fontSize = "1.4rem";
+    reactionBox.style.marginTop = "12px";
+    reactionBox.style.padding = "1rem";
+    reactionBox.style.borderRadius = "12px";
+
+    // create message
+    const reactionMessage = document.createElement("p");
+
+    if (storageItem.videos[videoId]) {
+        // reaction exists
+        const published = Date.parse(storageItem.videos[videoId].published_at);
+        reactionBox.style.backgroundColor = "#046931"; // green bg
+        reactionMessage.innerHTML = `<span style="font-weight: bold;">React gefunden: <a href="https://youtube.com/watch?v=${
+            storageItem.videos[videoId].reaction_id
+        }" target="_blank" style="color: white;">${
+            storageItem.videos[videoId].title
+        }</a></span><br>Hochgeladen: ${new Date(published).toLocaleDateString(
+            "de-DE"
+        )}`;
+    } else {
+        // reaction doesn't exist
+        reactionBox.style.backgroundColor = "#cc181e"; // red bg
+        reactionMessage.style.fontWeight = "bold";
+        reactionMessage.textContent = "Kein React gefunden";
+    }
+
+    // insert element
+    reactionBox.appendChild(reactionMessage);
+    aboveTheFold.insertBefore(reactionBox, aboveTheFold.children[2]);
+}
+
+// set local storage
+if (storageItem === null) {
+    console.log(logPreTag, "creating new storage item");
+    createStorageItem();
+} else {
+    console.log(logPreTag, "storage item found");
+    storageItem = JSON.parse(storageItem);
+    // fetch new videos if localstorage older than 1h
+    if (Date.now() - storageItem.fetched_at >= 3600000) {
+        console.log(logPreTag, "storage item outdated. creating new");
+        createStorageItem();
+    } else {
+        console.log(logPreTag, "storage item in time range");
+    }
+}
+
+// wait for youtube elements to be created
+const callback = function (mutationsList, observer) {
+    for (const mutation of mutationsList) {
+        if (
+            mutation.target.id === "bottom-row" &&
+            document.querySelector("#above-the-fold")
+        ) {
+            observer.disconnect();
+            createHTML();
+            return;
+        }
+    }
+};
+const observer = new MutationObserver(callback);
+observer.observe(document.body, { childList: true, subtree: true });
+
+// watch for SPA page event
+window.addEventListener(
+    "yt-navigate-finish",
+    () => {
+        if (document.querySelector("#above-the-fold")) {
+            createHTML();
+        }
+    },
+    true
+);
